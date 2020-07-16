f9568a511bee524d2b85ec3e5a04dcd446d7845e
Lauren Burroughs <lburroughs@phase2technology.com>
2020-07-15 22:12:08 -0500

    create an about page and a page title component.

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PageTitle from "../components/page-title"
+
+export default function About() {
+  return (
+    <div>
+      <PageTitle
+        text="Lauren Burroughs"
+      />
+      <p>Something something something.</p>
+    </div>
+  )
+}
