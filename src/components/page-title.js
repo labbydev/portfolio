3725d0cade52d3e3b910f4245ab30666ec3ac112
Lauren Burroughs <lburroughs@phase2technology.com>
2020-07-16 12:47:02 -0500

    Add Playfair Display and Spartan fonts.

--- src/components/page-title.js
+++ src/components/page-title.js
@@ -1,5 +1,7 @@
 import React from "react"
 
 export default function PageTitle(props) {
-  return <h1>{props.text}</h1>
+  return <h1
+    className={`font-serif`}
+  >{props.text}</h1>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [],
+  //@TODO: remove manual postcss-purge and configure built in purge.
+  purge: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+        sans: ["Spartan", ...defaultTheme.fontFamily.sans]
+      },
+    },
   },
   variants: {},
   plugins: [],
