d60e6d4caf6ec8ed6dc71ca2638e9fb144511ec4
Lauren Burroughs <lburroughs@phase2technology.com>
2020-07-31 11:33:23 -0500

    Use logo as favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Lauren Burroughs`,
+        short_name: `labbydev`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#F59D16`,
+        theme_color: false,
+        display: `standalone`,
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
