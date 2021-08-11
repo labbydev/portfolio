b56c421f9a6b28613af28b0169108e115b8d78e4
labbydev <labyrwa@gmail.com>
2021-08-11 17:40:24 +0000

    Deploying to gh-pages from  @ 7944edbf222455c672fb1a0b7b94cabe9496aa5c 🚀

---
+++ component---src-pages-404-js-88540ee46d224a451962.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),u=n(6943),o=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(o.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-88540ee46d224a451962.js.map
